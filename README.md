95117b3d335baf46824b75fcff42eca48cf8ad68
Xiangpeng Hao <xha62@sfu.ca>
2019-10-05 14:22:44 -0700

    add tests

--- README.md
+++ README.md
@@ -1,34 +1,23 @@
-# Template to bootstrap C++ research projects
+# Simple Epoch Based Memory Reclaimer
 
 [![Build Status](https://patrick-website.visualstudio.com/cpp-template/_apis/build/status/XiangpengHao.cpp-template?branchName=master)](https://patrick-website.visualstudio.com/cpp-template/_build/latest?definitionId=2&branchName=master)
 
-> this language (C++) has many dark corner, stupid conventions, implicit conversion and not mention UB
-> -- [Wojciech Muła](http://0x80.pl/notesen/2015-05-25-tricky-mistake.html)
-
-Context: my research focus is high performance computing
-
-You might also like: https://www.rust-lang.org. My intuition is unless your daily routine is to perf code line-by-line, calcuate each cache use, measure every memory hit; you should consider rust-lang.
+Code adapted from [PMwCAS](https://github.com/microsoft/pmwcas), all bugs are mine.
 
 ## Features
 
-- GTest + GLog
+- Small, simple 
 
-- Modern CMake, keep compile time in mind 
-
-- C++ 17
-
-- Sanitizers enabled in Debug mode, `march=native` in Release mode.
-
-- Docker enabled
-
-- CI included
-
-- Clang-format: Google style
+- Header-only
 
 ## Build
 
 ```bash
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE={Debug|Release} ..
-make -j
+make tests
 ```
+
+## Usage
+
+
--- tests/epoch_test.cpp
+++ tests/epoch_test.cpp
@@ -104,7 +104,10 @@ class MinEpochTableTest : public ::testing::Test {
 
   virtual void SetUp() { table_.Initialize(); }
 
-  virtual void TearDown() { table_.Uninitialize(); }
+  virtual void TearDown() {
+    table_.Uninitialize();
+    Thread::ClearRegistry(true);
+  }
 };
 
 TEST_F(MinEpochTableTest, Initialize) {
@@ -143,6 +146,121 @@ TEST_F(MinEpochTableTest, Protect) {
     EXPECT_EQ(0llu, local_entry.protected_epoch.load());
     EXPECT_EQ(0llu, local_entry.last_unprotected_epoch);
   }
+}
+
+TEST_F(MinEpochTableTest, Unprotect) {
+  EXPECT_TRUE(table_.Protect(99));
+  EXPECT_TRUE(table_.Unprotect(101));
+  uint64_t entrySlot = Murmur3_64(pthread_self()) % table_.size_;
+
+  // Make sure the slot got released and timestamped and that
+  // the thread still has the slot locked with it's id still there.
+  const MinEpochTable::Entry& entry = table_.table_[entrySlot];
+  EXPECT_EQ(pthread_self(), entry.thread_id.load());
+  EXPECT_EQ(0llu, entry.protected_epoch.load());
+  EXPECT_EQ(101llu, entry.last_unprotected_epoch);
+
+  // Make sure none of the other slots got touched.
+  for (uint64_t i = 0; i < table_.size_; ++i) {
+    const MinEpochTable::Entry& local_entry = table_.table_[i];
+    if (entrySlot == i) continue;
+    EXPECT_EQ(0lu, local_entry.thread_id.load());
+    EXPECT_EQ(0llu, local_entry.protected_epoch.load());
+    EXPECT_EQ(0llu, local_entry.last_unprotected_epoch);
+  }
+}
+
+TEST_F(MinEpochTableTest, ComputeNewSafeToReclaimEpoch) {
+  EXPECT_EQ(99llu, table_.ComputeNewSafeToReclaimEpoch(100));
+  table_.table_[0].protected_epoch = 1;
+  EXPECT_EQ(0llu, table_.ComputeNewSafeToReclaimEpoch(100));
+  table_.table_[1].protected_epoch = 100;
+  EXPECT_EQ(0llu, table_.ComputeNewSafeToReclaimEpoch(100));
+  table_.table_[0].protected_epoch = 0;
+  EXPECT_EQ(99llu, table_.ComputeNewSafeToReclaimEpoch(101));
+  table_.table_[table_.size_ - 1].protected_epoch = 98;
+  EXPECT_EQ(97llu, table_.ComputeNewSafeToReclaimEpoch(101));
+
+  table_.table_[0].protected_epoch = 0;
+  table_.table_[1].protected_epoch = 0;
+
+  std::random_device rd;
+  std::default_random_engine engine(rd());
+  std::uniform_int_distribution<Epoch> dist(1, 1000);
+  std::vector<Epoch> epochs;
+  for (uint64_t i = 0; i < table_.size_; ++i) {
+    epochs.emplace_back(dist(engine));
+    table_.table_[i].protected_epoch = epochs.back();
+  }
+  EXPECT_EQ(*std::min_element(epochs.begin(), epochs.end()) - 1,
+            table_.ComputeNewSafeToReclaimEpoch(1001));
+}
+
+TEST_F(MinEpochTableTest, getEntryForThread) {
+  // Make sure the table is clear.
+  for (uint64_t i = 0; i < table_.size_; ++i) {
+    EXPECT_EQ(0lu, table_.table_[i].thread_id.load());
+    EXPECT_EQ(0llu, table_.table_[i].protected_epoch.load());
+    EXPECT_EQ(0llu, table_.table_[i].last_unprotected_epoch);
+  }
+  MinEpochTable::Entry* entry = nullptr;
+  EXPECT_TRUE(table_.GetEntryForThread(&entry));
+  EXPECT_NE(nullptr, entry);
+  // Make sure the table is clear except the one new entry.
+  for (uint64_t i = 0; i < table_.size_; ++i) {
+    if (entry == &table_.table_[i]) continue;
+    EXPECT_EQ(0lu, table_.table_[i].thread_id.load());
+    EXPECT_EQ(0llu, table_.table_[i].protected_epoch.load());
+    EXPECT_EQ(0llu, table_.table_[i].last_unprotected_epoch);
+  }
+  EXPECT_EQ(pthread_self(), entry->thread_id.load());
+  EXPECT_EQ(0llu, entry->protected_epoch.load());
+  EXPECT_EQ(0llu, entry->last_unprotected_epoch);
+}
+
+TEST_F(MinEpochTableTest, getEntryForThread_OneSlotFree) {
+  for (uint64_t i = 0; i < table_.size_ - 1; ++i) table_.ReserveEntry(i, 1);
+  MinEpochTable::Entry* entry = nullptr;
+  EXPECT_TRUE(table_.GetEntryForThread(&entry));
+  EXPECT_NE(nullptr, entry);
+  EXPECT_EQ(entry, &table_.table_[table_.size_ - 1]);
+  EXPECT_EQ(pthread_self(), entry->thread_id.load());
+  EXPECT_EQ(0llu, entry->protected_epoch.load());
+  EXPECT_EQ(0llu, entry->last_unprotected_epoch);
+}
+
+TEST_F(MinEpochTableTest, reserveEntryForThread) {
+  for (uint64_t i = 0; i < table_.size_; ++i) {
+    EXPECT_EQ(0lu, table_.table_[i].thread_id.load());
+    EXPECT_EQ(0llu, table_.table_[i].protected_epoch.load());
+    EXPECT_EQ(0llu, table_.table_[i].last_unprotected_epoch);
+  }
+  MinEpochTable::Entry* entry = table_.ReserveEntryForThread();
+  EXPECT_NE(nullptr, entry);
+  // Make sure the table is clear except the one new entry.
+  for (uint64_t i = 0; i < table_.size_; ++i) {
+    if (entry == &table_.table_[i]) continue;
+    EXPECT_EQ(0lu, table_.table_[i].thread_id.load());
+    EXPECT_EQ(0llu, table_.table_[i].protected_epoch.load());
+    EXPECT_EQ(0llu, table_.table_[i].last_unprotected_epoch);
+  }
+  EXPECT_EQ(pthread_self(), entry->thread_id.load());
+  EXPECT_EQ(0llu, entry->protected_epoch.load());
+  EXPECT_EQ(0llu, entry->last_unprotected_epoch);
+}
+
+TEST_F(MinEpochTableTest, reserveEntry) {
+  EXPECT_EQ(0u, table_.table_[0].thread_id.load());
+  table_.ReserveEntry(0, 1);
+  EXPECT_EQ(1u, table_.table_[0].thread_id.load());
+  table_.ReserveEntry(0, 2);
+  EXPECT_EQ(1u, table_.table_[0].thread_id.load());
+  EXPECT_EQ(2u, table_.table_[1].thread_id.load());
+
+  table_.ReserveEntry(table_.size_ - 1, 3);
+  EXPECT_EQ(3u, table_.table_[table_.size_ - 1].thread_id.load());
+  table_.ReserveEntry(table_.size_ - 1, 4);
+  EXPECT_EQ(4u, table_.table_[2].thread_id.load());
 }
 
 int main(int argc, char** argv) {
