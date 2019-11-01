d65041d1b70f6a515fbd42cbe44bfc127cbafc10
Xiangpeng Hao <xha62@sfu.ca>
2019-10-31 22:46:00 -0700

    add base line

--- benchmark/pcas_bench.cpp
+++ benchmark/pcas_bench.cpp
@@ -1,21 +1,24 @@
 #include <glog/logging.h>
 #include <gtest/gtest.h>
 #include <libpmemobj.h>
+#include <memory>
 #include <random>
 #include "../pcas.h"
 #include "bench_common.h"
 
-POBJ_LAYOUT_BEGIN(pcas_layout);
-POBJ_LAYOUT_TOID(pcas_layout, char);
-POBJ_LAYOUT_END(pcas_layout);
+POBJ_LAYOUT_BEGIN(benchmark);
+POBJ_LAYOUT_TOID(benchmark, char);
+POBJ_LAYOUT_END(benchmark);
 
-struct PCASBench : public PerformanceTest {
-  const char* GetBenchName() override { return "PCASBench"; }
+static const constexpr uint32_t kItemCnt = 48 * 2;
+static const constexpr uint32_t kArrayLen = 1000;
+static const constexpr uint32_t kOpCnt = 10000;
 
+struct BaseBench : public PerformanceTest {
   PMEMobjpool* pool{nullptr};
-  PCASBench() {
+  BaseBench() {
     static const char* pool_name = "/mnt/pmem0/pcas_pool.data";
-    static const char* layout_name = "pcas_layout";
+    static const char* layout_name = "benchmark";
     static const uint64_t pool_size = 1024 * 1024 * 1024;
     if (!pm_tool::FileExists(pool_name)) {
       pool = pmemobj_create(pool_name, layout_name, pool_size,
@@ -25,55 +28,36 @@ struct PCASBench : public PerformanceTest {
     }
     EXPECT_NE(pool, nullptr);
   }
+  ~BaseBench() { pmemobj_close(pool); }
 
-  ~PCASBench() { pmemobj_close(pool); }
-
-  static const constexpr uint32_t item_cnt_ = 48 * 2;
-  static const constexpr uint32_t array_len = 1000;
-  static const constexpr uint32_t op_cnt_ = 10000;
-
-  uint64_t* array{nullptr};
   std::mt19937 rng{42};
 
-  void Entry(size_t thread_idx, size_t thread_count) override {
-    if (thread_idx == 0) {
-      PMEMoid ptr;
-      pmemobj_zalloc(pool, &ptr,
-                     pm_tool::PMDK_PADDING + sizeof(pm_tool::DirtyTable) +
-                         sizeof(pm_tool::DirtyTable::Item) * item_cnt_,
-                     TOID_TYPE_NUM(char));
-      auto table = (pm_tool::DirtyTable*)((char*)pmemobj_direct(ptr) +
-                                          pm_tool::PMDK_PADDING);
-      pm_tool::DirtyTable::Initialize(table, item_cnt_);
+  uint64_t* array{nullptr};
 
-      pmemobj_zalloc(
-          pool, &ptr,
-          pm_tool::PMDK_PADDING + sizeof(pm_tool::kCacheLineSize) * array_len,
-          TOID_TYPE_NUM(char));
-      array = (uint64_t*)((char*)pmemobj_direct(ptr) + pm_tool::PMDK_PADDING);
-      memset(array, 0, pm_tool::kCacheLineSize * array_len);
-    }
+  void WorkLoadInit() {
+    auto table = (pm_tool::DirtyTable*)ZAlloc(
+        sizeof(pm_tool::DirtyTable) +
+        sizeof(pm_tool::DirtyTable::Item) * kItemCnt);
+    pm_tool::DirtyTable::Initialize(table, kItemCnt);
+    array = (uint64_t*)ZAlloc(pm_tool::kCacheLineSize * kArrayLen);
+  }
 
-    std::uniform_int_distribution<std::mt19937::result_type> dist(
-        0, array_len - 1);
-    WaitForStart();
-
-    for (uint32_t i = 0; i < op_cnt_; i += 1) {
-      uint32_t pos = dist(rng);
-      uint64_t* target =
-          array + pos * pm_tool::kCacheLineSize / sizeof(uint64_t);
-      uint64_t value = *target;
-      pm_tool::PersistentCAS(target, value, value + 1);
-    }
+  void* ZAlloc(size_t size) {
+    PMEMoid ptr;
+    pmemobj_zalloc(pool, &ptr, pm_tool::PMDK_PADDING + size,
+                   TOID_TYPE_NUM(char));
+    auto abs_ptr = (char*)pmemobj_direct(ptr) + pm_tool::PMDK_PADDING;
+    // pmemobj_memset_persist(pool, abs_ptr, 0, size);
+    return abs_ptr;
   }
 
   void Teardown() override {
     uint64_t sum{0};
-    for (uint32_t i = 0; i < array_len; i += 1) {
+    for (uint32_t i = 0; i < kArrayLen; i += 1) {
       uint64_t* target = array + i * pm_tool::kCacheLineSize / sizeof(uint64_t);
       sum += *target;
     }
-    std::cout << "sum: " << sum << std::endl;
+    std::cout << "Succeeded CAS: " << sum << std::endl;
     auto oid = pmemobj_oid((char*)pm_tool::DirtyTable::GetInstance() -
                            pm_tool::PMDK_PADDING);
     pmemobj_free(&oid);
@@ -82,7 +66,65 @@ struct PCASBench : public PerformanceTest {
   }
 };
 
+struct PCASBench : public BaseBench {
+  const char* GetBenchName() override { return "PCASBench"; }
+
+  PCASBench() : BaseBench() {}
+
+  void Entry(size_t thread_idx, size_t thread_count) override {
+    if (thread_idx == 0) {
+      WorkLoadInit();
+    }
+
+    std::uniform_int_distribution<std::mt19937::result_type> dist(
+        0, kArrayLen - 1);
+
+    WaitForStart();
+
+    for (uint32_t i = 0; i < kOpCnt; i += 1) {
+      uint32_t pos = dist(rng);
+      uint64_t* target =
+          array + pos * pm_tool::kCacheLineSize / sizeof(uint64_t);
+      uint64_t value = *target;
+      pm_tool::PersistentCAS(target, value, value + 1);
+    }
+  }
+};
+
+struct CASBench : public BaseBench {
+  const char* GetBenchName() override { return "CASBench"; }
+  CASBench() : BaseBench() {}
+
+  void Entry(size_t thread_idx, size_t thread_count) override {
+    if (thread_idx == 0) {
+      WorkLoadInit();
+    }
+
+    std::uniform_int_distribution<std::mt19937::result_type> dist(
+        0, kArrayLen - 1);
+
+    WaitForStart();
+
+    for (uint32_t i = 0; i < kOpCnt; i += 1) {
+      uint32_t pos = dist(rng);
+      uint64_t* target =
+          array + pos * pm_tool::kCacheLineSize / sizeof(uint64_t);
+      uint64_t value = *target;
+
+      pm_tool::CompareExchange64(target, value + 1, value);
+      pm_tool::flush(target);
+      pm_tool::fence();
+    }
+  }
+};
+
 int main() {
-  auto pcas_bench = new PCASBench();
-  pcas_bench->Run(1)->Run(2)->Run(4)->Run(8);
+  {
+    auto pcas_bench = std::make_unique<PCASBench>();
+    pcas_bench->Run(1)->Run(2)->Run(4)->Run(8);
+  }
+  {
+    auto cas_bench = std::make_unique<CASBench>();
+    cas_bench->Run(1)->Run(2)->Run(4)->Run(8);
+  }
 }
